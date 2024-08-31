9828377091d40f3590a9e8a6e988a8631658acf0
Valerie Thibault <valerie@Valeries-Air.Home>
2024-08-31 15:39:45 +0100

    add to upm2sp.py

--- upm2sp.py
+++ upm2sp.py
@@ -5,6 +5,12 @@ def upm2sp(upm):
 def point2sp(x_upm, y_upm):
     return (upm2sp(x_upm), upm2sp(y_upm))
 
+"""
+Prints the glyphbBoxes record for a glyph.
+- glyph: string of the glyphname as defined by smufl
+- ne_upm: float tuple, the coordinates in upm for the north east corner
+- sw_upm: float tuple, the coordinates in upm for the north east corner
+"""
 def generate_bBox_JSON(glyph, ne_upm, sw_upm):
     ne_sp = point2sp(*ne_upm)
     sw_sp = point2sp(*sw_upm)
